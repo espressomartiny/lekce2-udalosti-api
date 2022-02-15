3027129fb348007f9e9a32a1932294611cb8d74f
Martina Vokáčová <martinavokacova@gmail.com>
2022-02-16 00:59:07 +0100

    styl

--- main.js
+++ main.js
@@ -1,79 +1,76 @@
 // tady je místo pro náš program
 
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+nadpis.classList.remove("zluty");
 
-let nadpis = document.querySelector('h1');
-nadpis.classList.add('zluty');
-nadpis.classList.remove('zluty');
-
-let ctverecek = document.querySelector('div.zluty')
-ctverecek.classList.add('ctverecek')
+let ctverecek = document.querySelector("div.zluty");
+ctverecek.classList.add("ctverecek");
 
 let pocitadlo = 0;
-function priNajeti () {
-console.log('test' + pocitadlo);
-pocitadlo++;
+function priNajeti() {
+  console.log("test" + pocitadlo);
+  pocitadlo++;
 }
 
 function zmenStyl() {
-    nadpis.classList.toggle('zluty')
+  nadpis.classList.toggle("zluty");
 }
-
-
 
 // úkoly
 
 function ztucniPriNajeti() {
-    let pali = document.querySelector('.palindrom');
-    pali.classList.add('mordnilap');
+  let pali = document.querySelector(".palindrom");
+  pali.classList.add("mordnilap");
 }
 
 function odtucniPriVyjeti() {
-    let pali = document.querySelector('.palindrom');
-    pali.classList.remove('mordnilap');
+  let pali = document.querySelector(".palindrom");
+  pali.classList.remove("mordnilap");
 }
 
 function zcervenej() {
-    let pali = document.querySelector('.palindrom');
-    pali.classList.toggle('cervena')
+  let pali = document.querySelector(".palindrom");
+  pali.classList.toggle("cervena");
 }
 
-let sizePalindromu = 17
+let sizePalindromu = 17;
 function zvets() {
-    let pali = document.querySelector('.palindrom');
-    sizePalindromu++
-    let px = sizePalindromu + 'px'
-    pali.style.fontSize = px;
+  let pali = document.querySelector(".palindrom");
+  sizePalindromu++;
+  let px = sizePalindromu + "px";
+  pali.style.fontSize = px;
 }
 
 function stisk(udalost) {
-    console.log(udalost.key)
+  console.log(udalost.key);
 }
 
 function prehraj() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    console.log('Zapínám píseň...');
-    audioFile.play();
+  let audioFile = document.getElementById("zvukova-stopa");
+  console.log("Zapínám píseň...");
+  audioFile.play();
 }
 
 function pauzni() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    audioFile.pause();
+  let audioFile = document.getElementById("zvukova-stopa");
+  audioFile.pause();
 }
 
 function hlasitostDown() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    audioFile.volume = 0;
+  let audioFile = document.getElementById("zvukova-stopa");
+  audioFile.volume = 0;
 }
 function hlasitostMid() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    audioFile.volume = 0.5;
+  let audioFile = document.getElementById("zvukova-stopa");
+  audioFile.volume = 0.5;
 }
 function hlasitostUp() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    audioFile.load = 1;
+  let audioFile = document.getElementById("zvukova-stopa");
+  audioFile.load = 1;
 }
 
 function vrat() {
-    let audioFile = document.getElementById('zvukova-stopa');
-    audioFile.currentTime = 0;
+  let audioFile = document.getElementById("zvukova-stopa");
+  audioFile.currentTime = 0;
 }
