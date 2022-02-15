3ba0646f1fe7208b9105b6865c1c325de0c8f8dd
Martina Vokáčová <martinavokacova@gmail.com>
2022-02-15 19:31:18 +0100

    fce tučného palindromu

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 // tady je místo pro náš program
-console.log("čus")
+
+
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty');
+nadpis.classList.remove('zluty');
+
+let ctverecek = document.querySelector('div.zluty')
+ctverecek.classList.add('ctverecek')
+
+
+
+let pocitadlo = 0;
+function priNajeti () {
+console.log('test' + pocitadlo);
+pocitadlo++;
+}
+
+function zmenStyl() {
+    nadpis.classList.toggle('zluty')
+}
+
+// úkol na lekci
+
+let pali = document.querySelector('.palindrom');
+
+function ztucniPriNajeti() {
+pali.classList.add('mordnilap');
+}
